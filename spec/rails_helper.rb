838f385a48e5c8bcb85064787860923101c753b5
Jeferson Silva <jefersonsilva.contato@gmail.com>
2026-04-17 00:53:30 -0300

    adiciona suporte a testes para a API e corrige caminhos de arquivos

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-Dir[Rails.root.join('spec/suport/**/*.rb')].each { |file| require file }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
---
+++ spec/requests/admin/v1/home_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Home API", type: :request do
+  let(:user) { create(:user) }
+    it "Test Home" do
+      get "/admin/v1/home", headers: auth_header(user)
+
+      expect(body_json).to eq({'message' => 'Hello World' })
+    end
+
+    it "Test Home" do
+      get "/admin/v1/home", headers: auth_header(user)
+
+      expect(response).to have_http_status(:ok)
+    end
+end
